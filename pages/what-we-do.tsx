d0b377684ef207061d5c4b6ccb68df10e5fb95f0
Bioga Allen <allenojuka10@gmail.com>
2022-07-18 18:08:49 +0300

    Build what we do page.

--- pages/what-we-do.tsx
+++ pages/what-we-do.tsx
@@ -13,7 +13,18 @@ const WhatWeDo: NextPage = ({
         desc: 'Our building and construction products and services.'
       }}
     >
-
+      <div className="grid grid-col-1 md:grid-cols-2 gap-6 flex-wrap lg:w-[60%] xl:w-[50%]
+      h-full py-12 pr-4 overflow-y-scroll">
+        {data?.sub_sections.map((section: any) => (
+          <div
+            key={section.id}
+            className='rounded-xl bg-gray-900/90 border border-gray-700 p-6'
+          >
+            <h2 className="text-4xl font-semibold tracking-tight">{section.title}</h2>
+            <p className="mt-4 text-gray-300">{section.content}</p>
+          </div>
+        ))}
+      </div>
     </Page>
   )
 }
@@ -21,7 +32,22 @@ const WhatWeDo: NextPage = ({
 export default WhatWeDo
 
 interface IData {
+  id: number,
+  url: string,
+  title: string,
+  content: string,
+  image: string,
+  sub_sections: ISubSection[]
+}
 
+interface ISubSection {
+  id: number,
+  section: number,
+  title: string,
+  sub_title: string,
+  content: string,
+  image: string,
+  date_created: string
 }
 
 export const getStaticProps: GetStaticProps = async () => {
