dbb51e96a76ce2649cf696e2ac82a89312047262
Bioga Allen <allenojuka10@gmail.com>
2022-07-18 19:02:05 +0300

    Removed transition and duration.

--- components/NavLink/index.tsx
+++ components/NavLink/index.tsx
@@ -21,7 +21,7 @@ const NavLink = ({
     <Link href={path} passHref>
       <a href="" className={`uppercase text-xs text-white hover:bg-white
     hover:text-gray-900 tracking-widest px-6 py-4 cursor-pointer ${activeStyles}
-    duration-200 transition`}>
+    `}>
         {name}
       </a>
     </Link>
